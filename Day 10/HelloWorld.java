744be5a33bcf54bfc74c15f9bd6b581d4e7ee53f
Angel <GoodTimeCruz@onthecruz.local>
2019-06-11 15:12:00 -0400

    new folder

---
+++ Day 10/HelloWorld.java
@@ -0,0 +1,17 @@
+//[access modifier][non-access modifier ] class [className]{}
+//public is an access modifier
+
+public class HelloWorld{
+
+    public static void main(String[] args) {
+        //print out to console
+        System.out.println("yurrrr !");
+
+    }
+}
+/* Welcome to Java! This is our first Java class
+
+It is important to note that the name of the .
+java file must be the same name as the public class in that file*/
+
+//This is single line comment
